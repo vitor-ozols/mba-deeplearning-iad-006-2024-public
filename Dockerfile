ce20f2c3c3e9b9677732af984fdfcbc8bf083f65
vitor-ozols <108703032+vitor-ozols@users.noreply.github.com>
2024-08-21 01:26:57 +0000

    Ajustes finais para deploy com docker e REQ de teste

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,26 @@
+# Use uma imagem oficial do Python 3.9 com tamanho reduzido
 FROM python:3.9-slim
 
-WORKDIR /
+# Defina o diretório de trabalho dentro do container
+WORKDIR /app
 
-COPY . /
+# Copie apenas os arquivos necessários para o container
+COPY requirements.txt ./
 
+# Instale as dependências listadas no requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
 
-EXPOSE 8080
+# Copie a pasta models e o restante dos arquivos da aplicação
+COPY models/ ./models/
+COPY . .
 
-ENV FLASK_APP=app.py
-ENV FLASK_ENV=production
+# Exponha a porta 8080 para acesso externo
+EXPOSE 8000
 
-# Especificar a porta 8080 no comando do Flask
-CMD ["flask", "run", "--host=0.0.0.0", "--port=8080"]
+# Configure variáveis de ambiente para a aplicação Flask
+ENV FLASK_APP=./app.py
+ENV FLASK_RUN_HOST=0.0.0.0
+ENV FLASK_RUN_PORT=8000
+
+# Comando para iniciar a aplicação Flask, vinculando à porta 8080
+CMD ["flask", "run", "--host=0.0.0.0", "--port=8000"]
--- app.py
+++ app.py
@@ -7,23 +7,27 @@ import pickle
 # Inicializar a aplicação Flask
 app = Flask(__name__)
 
-# carregar o conjunto de dados e normalizar os dados
+# Carregar o conjunto de dados e normalizar os dados
 digits = load_digits()
 X, y = digits.data, digits.target
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
-# carregar o modelo salvo com pickle
-with open('../models/decision_tree_model.pkl', 'rb') as model_file:
+# Carregar o modelo salvo com pickle
+with open('models/decision_tree_model.pkl', 'rb') as model_file:
     clf = pickle.load(model_file)
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json.get('data')
+    if len(data) != X.shape[1]:
+        return jsonify({'error': f'Expected {X.shape[1]} features, but got {len(data)}'}), 400
+    
     data = np.array(data).reshape(1, -1)
     data_scaled = scaler.transform(data)
     prediction = clf.predict(data_scaled)
     return jsonify({'prediction': int(prediction[0])})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Execute a aplicação Flask na porta 8000
+    app.run(debug=True, host='0.0.0.0', port=8000)
